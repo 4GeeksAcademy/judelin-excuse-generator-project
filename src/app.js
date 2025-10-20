ab5a71cac04f04c346470630ca6420a6c4feb293
cinjude <110071132+cinjude@users.noreply.github.com>
2025-10-20 12:49:53 +0000

    [I created the function in the app.js so that I could get the random value and add everything needed in HTML and CSS]

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car', 'the tv'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getRandomAleatorio(allArray){
+  let random = Math.floor(Math.random()* allArray.length);
+  return allArray[random]
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = getRandomAleatorio(who) + " " + getRandomAleatorio(action)+ " " + getRandomAleatorio(what) + " " + getRandomAleatorio(when);
+
+
 };
