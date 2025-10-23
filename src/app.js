08d1c596186a2a27106a0deb7668a798b0680496
cinjude <110071132+cinjude@users.noreply.github.com>
2025-10-23 01:25:40 +0000

    [I modified the JavaScript file, index.html, and styles.css.] I modified what was necessary so that I could maintain a good practice example of a variable when it exceeds more than 80 columns, then in the class that is in the div tag.

--- src/app.js
+++ src/app.js
@@ -8,16 +8,21 @@ import "./assets/img/4geeks.ico";
 let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car', 'the tv'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+let when = [
+  'before the class', 'when I was sleeping',
+  'while I was exercising', 'during my lunch', 'while I was praying'
+];
 
-function getRandomAleatorio(allArray){
-  let random = Math.floor(Math.random()* allArray.length);
+function getRandomAleatorio(allArray) {
+  let random = Math.floor(Math.random() * allArray.length);
   return allArray[random]
 }
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  document.querySelector("#excuse").innerHTML = getRandomAleatorio(who) + " " + getRandomAleatorio(action)+ " " + getRandomAleatorio(what) + " " + getRandomAleatorio(when);
+  document.querySelector("#excuse").innerHTML = getRandomAleatorio(who)
+    + " " + getRandomAleatorio(action) + " " + getRandomAleatorio(what)
+    + " " + getRandomAleatorio(when);
 
 
 };
